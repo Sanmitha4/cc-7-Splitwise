afcaba3c0cd01b573bc83cf768a01b9b45a9e322
Sanmitha4 <sanmithashetty2004@gmail.com>
2026-03-31 12:29:10 +0000

    Added the repository

---
+++ src/controller/friends.controller.ts
@@ -0,0 +1,13 @@
+import type friendModel = require("../models/friend-model");
+
+export class FriendsController{
+    checkEmailExists(email:string){
+        return false;
+    }
+    checkPhoneExists(phone:string){
+        return false;
+    }
+    addFriend(friend:friendModel.Friend){
+        console.log('Adding friend to database...',friend);
+    }
+}
---
+++ src/repositories/friends.repository.ts
@@ -0,0 +1,28 @@
+class FriendsRepository{
+    private static instance:FriendsRepository;
+    static getInstance(){
+        if(!FriendsRepository.instance){
+            FriendsRepository.instance=new FriendsRepository();
+
+        }
+        return FriendsRepository.instance;
+
+    }
+    private constructor(){};
+    addFriend(friend:Friend){
+        this.friends.push(friend);
+        console.log('Friend added to repository:',friend);
+    }
+    findFriendByEmail(email:string){
+        return this.friends.find(friend=>friend.)
+    }
+    findFriendByPhone(email:string){
+        )
+    }
+searchFriends(lowercase)
+friends.name
+friends.email
+friends.phone
+
+
+export const friendsRepository=new FriendsRepository();
