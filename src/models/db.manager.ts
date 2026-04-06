0c205c85ef6ea97b526de28fd7036d38e27bf9da
Sanmitha4 <sanmithashetty2004@gmail.com>
2026-04-06 08:22:01 +0000

    Implementation of AppDBManager

--- src/models/db.manager.ts
+++ src/models/db.manager.ts
@@ -1,67 +1,28 @@
-// import {
-//   Database,
-//   type Dataset,
-//   type Row,
-//   JsonAdapter,
-// } from "../core/storage/db.js";
 
-// import type { Friend } from "./friend-model.js";
-// //import type { Expense } from '../../models/expense-model.js'
-
-// // interface AppData extends Dataset {
-// //     friends: Friend[];
-// //     expenses: Expense[]; // Add the expenses table
-// // }
-// interface AppData extends Dataset {
-//   friends: Friend[];
-// }
-
-// export class AppDBManager {
-//   private constructor() {
-//     this.db = new Database<AppData, keyof AppData>(
-//       "../../data/data.json",
-//       JsonAdapter,
-//     );
-//   }
-//   private static sharedInstance: AppDBManager | undefined = undefined;
-//   private db: Database<AppData, keyof AppData>;
-
-//   static getInstance(): AppDBManager {
-//     if (!this.sharedInstance) {
-//       this.sharedInstance = new AppDBManager();
-//     }
-//     return this.sharedInstance;
-//   }
-
-//   getDB() {
-//     return this.db;
-//   }
-  
-//   save() {
-//     this.db.save();
-//   }
-// }
-
-import path from 'path';
+import path from "path";
 import {
   Database,
-  type Dataset,
-  type Row,
-  JsonAdapter,
-} from '../core/storage/db.js';
-import type { Friend } from './friend-model.js';
+  JsonAdapter, 
+  type Table,
+} from "../core/storage/db.js";
+import type { Friend } from "./friend.model.js";
 
-interface AppData extends Dataset {
+interface AppData extends Record<string, Table> {
   friends: Friend[];
 }
 
 export class AppDBManager {
-    private constructor() {
-      const dbPath=path.resolve(process.cwd(),'data/data.json');
-    this.db = new Database<AppData, keyof AppData>(dbPath,JsonAdapter);
-  }
+  private db: Database<AppData>;
   private static sharedInstance: AppDBManager | undefined = undefined;
-  private db: Database<AppData, keyof AppData>;
+
+  private constructor() {
+    const dbPath = path.resolve(process.cwd(),'data/data.json');// "data", "data.json");
+//../../data/data.json if needed to be add in dist
+
+    const adapter = new JsonAdapter<AppData>();
+
+    this.db = new Database<AppData>(dbPath, adapter);
+  }
 
   static getInstance(): AppDBManager {
     if (!this.sharedInstance) {
